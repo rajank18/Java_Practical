daae997d7b380841ff54d57bfc4d6a7ca9c8fef2
Rajan Kanzariya <rajankanzariya1806@gmail.com>
2024-09-03 09:14:10 +0530

    modified

--- prac31.java
+++ prac31.java
@@ -1,27 +1,27 @@
 public class prac31 {
 
-    // Method A: By Extending the Thread Class
-    static class HelloWorldThread extends Thread {
+    
+    static class demo extends Thread {
         public void run() {
             System.out.println("Hello from Thread class");
         }
     }
 
-    // Method B: By Implementing the Runnable Interface
-    static class HelloWorldRunnable implements Runnable {
+   
+    static class demo1 implements Runnable {
         public void run() {
             System.out.println("Hello from Runnable interface");
         }
     }
 
     public static void main(String[] args) {
-        // Creating and starting a thread by extending Thread class
-        HelloWorldThread thread1 = new HelloWorldThread();
-        thread1.start();  // Starts the thread, which calls the run() method of HelloWorldThread
+        
+        demo thread1 = new demo();
+        thread1.start(); 
 
-        // Creating and starting a thread by implementing Runnable interface
-        HelloWorldRunnable runnable = new HelloWorldRunnable();
+      
+        demo1 runnable = new demo1();
         Thread thread2 = new Thread(runnable);
-        thread2.start();  // Starts the thread, which calls the run() method of HelloWorldRunnable
+        thread2.start(); 
     }
 }
